3d2911fd391a071005d346c87740a8fa1d95a99a
MatiasTesioWolox <matias.tesio@wolox.com.ar>
2018-10-04 16:21:09 -0300

    Adding destructuring on square props

--- my-app/src/app/screens/Game/components/Board/components/Square/index.js
+++ my-app/src/app/screens/Game/components/Board/components/Square/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Square(props) {
+function Square({value, onClick, ...props}) {
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
+    <button className="square" onClick={onClick}>
+      {value}
     </button>
   );
 }
