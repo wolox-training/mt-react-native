996afe7e6a5768b597ed63567650c19b83f6e1e2
MatiasTesioWolox <matias.tesio@wolox.com.ar>
2018-10-05 14:55:42 -0300

    Adding the provider

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import Game from './app/screens/Game';
 import './index.css';
+import store from './redux/store';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+const App = () => (
+  <Provider store={store}>
+    <Game />
+  </Provider>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
