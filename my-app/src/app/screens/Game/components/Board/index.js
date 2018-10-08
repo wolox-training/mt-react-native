f0a937508301e0b48216e82b81b820c53c28d8a0
MatiasTesioWolox <matias.tesio@wolox.com.ar>
2018-10-08 11:41:26 -0300

    fixing the array prototype validation

--- my-app/src/app/screens/Game/components/Board/index.js
+++ my-app/src/app/screens/Game/components/Board/index.js
@@ -35,7 +35,7 @@ class Board extends Component {
 }
 
 Board.propTypes = {
-  squares: PropTypes.array.isRequired,
+  squares: PropTypes.arrayOf(PropTypes.number).isRequired,
   onClick: PropTypes.func.isRequired
 };
 
