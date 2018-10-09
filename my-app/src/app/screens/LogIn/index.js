97f99501635bc80e1711d3d2418be98432ef02cd
MatiasTesioWolox <matias.tesio@wolox.com.ar>
2018-10-09 17:18:33 -0300

    adding initial values to customInputFields

--- my-app/src/app/screens/LogIn/index.js
+++ my-app/src/app/screens/LogIn/index.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import LogIn from './layout';
 
 class RegisterFromContainer extends Component {
+  getInitialValues = () => {
+    return {
+      mail: 'example@wolox.com.ar',
+      password: 'ponele'
+    };
+  };
+
   submit = values => {
     window.alert(JSON.stringify(values, null, 4));
   };
+
   render() {
-    return <LogIn onSubmit={this.submit} />;
+    return <LogIn onSubmit={this.submit} initialValues={this.getInitialValues()} />;
   }
 }
 
