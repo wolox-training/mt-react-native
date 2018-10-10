44d8d55609bb4910e61e88dd54ac7860e00ff2d0
MatiasTesioWolox <matias.tesio@wolox.com.ar>
2018-10-10 09:55:35 -0300

    Changing the App arrow funtion to a traditional styled funciton

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,10 +6,12 @@ import Game from './app/screens/Game';
 import './index.css';
 import { store } from './redux/store';
 
-const App = () => (
-  <Provider store={store}>
-    <Game />
-  </Provider>
-);
+function App() {
+  return (
+    <Provider store={store}>
+      <Game />
+    </Provider>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
