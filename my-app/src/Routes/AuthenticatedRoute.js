4db26a538b447650c6e8215364bff61e810bfc03
MatiasTesioWolox <matias.tesio@wolox.com.ar>
2018-10-17 10:27:21 -0300

    changing AuthenticatedRoute function notation

--- my-app/src/Routes/AuthenticatedRoute.js
+++ my-app/src/Routes/AuthenticatedRoute.js
@@ -4,9 +4,9 @@ import { connect } from 'react-redux';
 import Game from '../app/screens/Game/index';
 import RegisterFromContainer from '../app/screens/LogIn/index';
 
-const AuthenticatedRoute = props => {
-  return props.auth !== '' ? <Game /> : <RegisterFromContainer />;
-};
+function AuthenticatedRoute({ auth }) {
+  return auth !== '' ? <Game /> : <RegisterFromContainer />;
+}
 
 const mapStateToProps = state => ({
   auth: state.login.authorization
