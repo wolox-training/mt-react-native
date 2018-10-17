74e2587045e5eda32eabf8502f56bdf137cb44a2
MatiasTesioWolox <matias.tesio@wolox.com.ar>
2018-10-17 10:36:44 -0300

    moving api request to its own file in services

--- my-app/src/redux/login/actions.js
+++ my-app/src/redux/login/actions.js
@@ -1,27 +1,11 @@
-import api from '../../config/api';
+import getAuthenticatedLogIn from '../../services/LogInService';
 
 export const actions = {
   LOG_IN: '@@LOG_IN/LOG_IN'
 };
 
-function setCredentialsInLocalStorage(response) {
-  localStorage.setItem('authorization', response.data.token);
-}
-
 export const makeALogInRequest = values => {
   return dispatch => {
-    const queryParams = { user: values.mail, password: values.password };
-    api.get('/users', queryParams).then(response => {
-      if (response.ok && response.data.length > 0) {
-        setCredentialsInLocalStorage(response);
-        api.setHeaders({ Authorization: response.data.token });
-        dispatch({
-          type: actions.LOG_IN,
-          payload: { authorization: response.data.token }
-        });
-      } else {
-        window.alert('Credenciales incorrectas, por favor intentelo nuevamente.');
-      }
-    });
+    getAuthenticatedLogIn(values, dispatch);
   };
 };
---
+++ my-app/src/services/LogInService.js
@@ -0,0 +1,24 @@
+import api from '../config/api';
+import { actions } from '../redux/login/actions';
+
+function setCredentialsInLocalStorage(response) {
+  localStorage.setItem('authorization', response.data.token);
+}
+
+function getAuthenticatedLogIn(values, dispatch) {
+  const queryParams = { user: values.mail, password: values.password };
+  api.get('/users', queryParams).then(response => {
+    if (response.ok && response.data.length > 0) {
+      setCredentialsInLocalStorage(response);
+      api.setHeaders({ Authorization: response.data.token });
+      dispatch({
+        type: actions.LOG_IN,
+        payload: { authorization: response.data.token }
+      });
+    } else {
+      window.alert('Credenciales incorrectas, por favor intentelo nuevamente.');
+    }
+  });
+}
+
+export default getAuthenticatedLogIn;
