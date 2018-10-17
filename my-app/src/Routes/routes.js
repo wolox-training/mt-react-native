6246d6b3817ca2b45ebc072bbbb57bc0f601a6fc
MatiasTesioWolox <matias.tesio@wolox.com.ar>
2018-10-17 09:51:23 -0300

    Fixing cx and funciton notation in routes

--- my-app/src/Routes/routes.js
+++ my-app/src/Routes/routes.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import AuthenticatedRounter from './AuthenticatedRoute';
 
-const Routes = () => {
+function Routes() {
   return (
     <Router>
       <Switch>
@@ -12,6 +12,6 @@ const Routes = () => {
       </Switch>
     </Router>
   );
-};
+}
 
 export default Routes;
--- my-app/src/app/screens/LogIn/Components/Fields/index.js
+++ my-app/src/app/screens/LogIn/Components/Fields/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import cx from 'classnames';
 
 import styles from './styles.scss';
 
 export const customInput = props => {
   const { label, input, type, meta } = props;
   return (
-    <div className={cx(styles.customInputContainer)}>
+    <div className={styles.customInputContainer}>
       <input {...props.input} type={props.type} />
       <label>{props.label}</label>
       {meta.error &&
         meta.touched &&
-        !meta.active && <div className={cx(styles.errorText)}> {props.label + meta.error}</div>}
+        !meta.active && <div className={styles.errorText}> {props.label + meta.error}</div>}
     </div>
   );
 };
