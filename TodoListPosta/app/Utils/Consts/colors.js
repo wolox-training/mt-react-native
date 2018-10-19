39691ee01308f9d9b623efec649dddf4f68e1666
MatiasTesioWolox <matias.tesio@wolox.com.ar>
2018-10-19 16:51:16 -0300

    fixing the checkbox box color

---
+++ TodoListPosta/app/Utils/Consts/colors.js
@@ -0,0 +1,6 @@
+const styleColors = {
+  checkboxBackgroundColor: 'rgba(0,0,0,0.8)',
+  boxBorderColor: 'pink'
+};
+
+export default styleColors;
--- TodoListPosta/app/components/checkbox/styles.js
+++ TodoListPosta/app/components/checkbox/styles.js
@@ -1,16 +1,17 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../Utils/Consts/colors';
 
 const styles = StyleSheet.create({
   box: {
     height: 20,
     width: 20,
     borderWidth: 2,
-    borderColor: 'black'
+    borderColor: colors.boxBorderColor
   },
   inner: {
     flex: 1,
     margin: 2,
-    backgroundColor: 'rgba(0,0,0,0.8)'
+    backgroundColor: colors.checkboxBackgroundColor
   }
 });
 
